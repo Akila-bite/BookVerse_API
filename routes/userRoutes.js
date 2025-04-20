c3a7f099476e3ea0e6919f11abbfdc6d3c52bc77
Nompumelelo <akilaprofvictoria@gmail.com>
2025-04-20 08:52:09 +0200

    login route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -21,31 +21,24 @@ router.post(
       throw new Error("Please provide all required fields");
     }
 
-    // Check if user already exists
     const existingUser = await User.findOne({ email });
     if (existingUser) {
       res.status(400);
       throw new Error("User already exists");
     }
 
-    // Hash the password
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
-    // Create new user
     const newUser = new User({
       username,
       email,
       password: hashedPassword,
     });
 
-    // Save new user to the database
     const savedUser = await newUser.save();
-
-    // Generate JWT token
     const token = generateToken(savedUser._id);
 
-    // Send the token and success message in the response
     res.status(201).json({
       message: "User registered successfully!",
       token,
@@ -53,6 +46,41 @@ router.post(
   })
 );
 
+/*
+ * @route   POST /api/users/login
+ * @desc    Authenticate user & get token
+ * @access  Public
+ */
+router.post(
+  "/login",
+  asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      res.status(400);
+      throw new Error("Please enter both email and password");
+    }
+
+    const user = await User.findOne({ email });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      res.status(200).json({
+        message: "Login successful",
+        user: {
+          _id: user._id,
+          username: user.username,
+          email: user.email,
+        },
+        token: generateToken(user._id),
+      });
+    } else {
+      res.status(401);
+      throw new Error("Invalid email or password");
+    }
+  })
+);
+
 module.exports = router;
+
 
 
